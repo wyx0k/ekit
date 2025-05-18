161499fa745b97d7d951ceb4f372a6502bb6a1d7
wyx0k <wangyxok@126.com>
2025-05-18 15:50:03 +0800

    add simple runnable component

--- app/component_default.go
+++ app/component_default.go
@@ -1,5 +1,9 @@
 package app
 
+import (
+	"context"
+)
+
 type SimpleComponent struct {
 	app  *AppContext
 	conf *ConfContext
@@ -13,4 +17,34 @@ func (g *SimpleComponent) Init(app *AppContext, conf *ConfContext) error {
 
 func (g *SimpleComponent) Close() error {
 	return nil
+}
+
+type SimpleRunnableComponent struct {
+	app    *AppContext
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+func (r *SimpleRunnableComponent) Init(app *AppContext, conf *ConfContext) error {
+	r.app = app
+	r.ctx, r.cancel = context.WithCancel(context.Background())
+	return nil
+}
+
+func (r *SimpleRunnableComponent) Close() error {
+	return nil
+}
+
+func (r *SimpleRunnableComponent) Run(app *AppContext, conf *ConfContext) error {
+	<-r.ctx.Done()
+	return nil
+}
+
+func (r *SimpleRunnableComponent) OnExit() error {
+	r.cancel()
+	return nil
+}
+
+func (r *SimpleRunnableComponent) Done() <-chan struct{} {
+	return r.ctx.Done()
 }
